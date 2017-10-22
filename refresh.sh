7227c19b1288b98d0fd5f7aec94d1a277b2015e2
spencerpeters <spencer@goebelpeters.com>
2017-10-22 17:26:43 +0200

    made a clone of the blog3 site

---
+++ refresh.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Temporarily store uncommited changes
+git stash
+
+# Verify correct branch
+git checkout develop
+
+# Build new files
+stack exec myblog clean
+stack exec myblog build
+
+# Get previous files
+git fetch --all
+git checkout -b master --track origin/master
+
+# Overwrite existing files with new files
+rsync -a --filter='P docs/'      \
+         --filter='P _cache/'     \
+         --filter='P .git/'       \
+         --filter='P .gitignore'  \
+         --filter='P .stack-work' \
+         --delete-excluded        \
+         docs/ .
+
+# Commit
+git add -A
+git commit -m "Publish."
+
+# Push
+git push origin master:master
+
+# Restoration
+git checkout develop
+git branch -D master
+git stash pop
